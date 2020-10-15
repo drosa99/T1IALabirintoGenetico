c61db2458d2339744c6cd13d0d069559fa033fc5
Daniela Amaral <danielarosa2309@gmail.com>
2020-10-15 12:30:07 -0300

    Comentarios e refatoracoes

--- t1/src/AEstrela.java
+++ t1/src/AEstrela.java
@@ -10,127 +10,129 @@ public class AEstrela {
 		public static final String ANSI_WHITE = "\u001b[37m";
 		public static final String ANSI_RESET = "\u001b[0m";
 
-		private final int[][] maze;
-		private final int size;
+		private final int[][] labirinto;
+		private final int tamanho;
 
-		private final Nodo start;
-		private final Nodo end;
-		private List<Nodo> path;
+		private final Nodo inicio;
+		private final Nodo fim;
+		private List<Nodo> caminho;
 
-		private final Set<Nodo> closedSet;
-		private final Set<Nodo> openSet;
-		private final HashMap<Nodo, Nodo> cameFrom;
+		private final Set<Nodo> nodosFechados;
+		private final Set<Nodo> nodosAbertos;
+		private final HashMap<Nodo, Nodo> veioDe;
 		private final HashMap<Nodo, Integer> gScore;
 		private final HashMap<Nodo, Integer> fScore;
 
-		public AEstrela(int[][] maze, int posX, int posY, int posXf, int posYf, int size) throws Exception {
-				this.maze = maze;
-				this.size = size;
-				this.start = new Nodo(posX, posY);
-				this.end = new Nodo(posXf, posYf);
-				this.path = new LinkedList<>();
+		public AEstrela(int[][] labirinto, int posX, int posY, int posXf, int posYf, int tamanho) throws Exception {
+				this.labirinto = labirinto;
+				this.tamanho = tamanho;
+				this.inicio = new Nodo(posX, posY);
+				this.fim = new Nodo(posXf, posYf);
+				this.caminho = new LinkedList<>();
 
-				this.closedSet = new HashSet<>();
-				this.openSet = new HashSet<>();
-				openSet.add(start);
-				this.cameFrom = new HashMap<>();
+				this.nodosFechados = new HashSet<>();
+				this.nodosAbertos = new HashSet<>();
+				nodosAbertos.add(inicio);
+				this.veioDe = new HashMap<>();
 				this.gScore = new HashMap<>();
-				this.gScore.put(start, 0);
+				this.gScore.put(inicio, 0);
 				this.fScore = new HashMap<>();
-				this.fScore.put(start, heuristica(start));
+				this.fScore.put(inicio, heuristica(inicio));
 		}
 
 		public void executa() throws Exception {
 				Optional<Nodo> _atual = Optional.empty();
-				while (!openSet.isEmpty()) {
-						_atual = openSet.stream().min(Comparator.comparingInt(fScore::get)); //pega o nodo aberto com o menor score da heuristica -> mais promissor
+				int i = 1;
+				while (!nodosAbertos.isEmpty()) {
+						_atual = nodosAbertos.stream().min(Comparator.comparingInt(fScore::get)); //pega o nodo aberto com o menor score da heuristica -> mais promissor
 						Nodo atual = _atual.get();
 
 						Thread.sleep(250);
 						System.out.println("---------------------------------" + ANSI_RESET);
+						System.out.println("Passo: " + i);
 						try {
 								printMazeWithAStarPath(atual);
+								i++;
 						} catch (Exception e) {
 								e.printStackTrace();
 						}
 
-						if (atual.equals(end))
+						if (atual.equals(fim))
 								break;
 
-						openSet.remove(atual); //como vai explorar todos os filhos, tira dos nodos abertos e coloca nos fechados
-						closedSet.add(atual);
+						nodosAbertos.remove(atual); //como vai explorar todos os filhos, tira dos nodos abertos e coloca nos fechados
+						nodosFechados.add(atual);
 
 						getVizinhos(atual).stream()
-								.filter(v -> closedSet.stream().noneMatch(c -> c.equals(v)))
-								.forEach(v -> { //visita todos os vizinhos que ainda nao foram abertos
-										int tentativeScore = gScore.get(atual) + 1; //score de tentativas
+								.filter(vizinho -> nodosFechados.stream().noneMatch(nodoFechado -> nodoFechado.equals(vizinho)))
+								.forEach(vizinho -> { //visita todos os vizinhos que ainda nao foram abertos
+										int tentativaScore = gScore.get(atual) + 1; //score de tentativas
 
-										if (openSet.stream().noneMatch(o -> o.equals(v))) {  //se o nodo ainda nao foi aberto -> abre ele
-												openSet.add(v);
+										if (nodosAbertos.stream().noneMatch(nodoAberto -> nodoAberto.equals(vizinho))) {  //se o nodo ainda nao foi aberto -> abre ele
+												nodosAbertos.add(vizinho);
+										}
 
-												if (gScore.keySet().stream().noneMatch(k -> k.equals(v))) //todo ver se isso precisa
-														fScore.put(v, Integer.MAX_VALUE);
-
-												if (gScore.keySet().stream().noneMatch(k -> k.equals(v))) //usado depois pra evitar loop -> talvez possa ser tirado por causa do orElse(Integer.max)
-														gScore.put(v, Integer.MAX_VALUE);
-
-										} else if (tentativeScore >= gScore.keySet().stream()
-												.filter(k -> k.equals(v))
+										//se o nodo ja esta aberto, e ja foi avaliado como vizinho de alguem,
+										//sai da execucao e nao avalia ele, para evitar que o nodo esteja duplicado no dicionaraio veioDe
+										else if (tentativaScore >= gScore.keySet().stream()
+												.filter(k -> k.equals(vizinho))
 												.map(k -> gScore.get(k))
-												.findFirst().orElse(Integer.MAX_VALUE)) { //vem aqui se o nodo esta aberto -> pode ser pra evitar loop entre irmaos
+												.findFirst().orElse(Integer.MAX_VALUE)) {
 												return;
 										}
-										cameFrom.put(v, atual); //atribui relacao pai e filho -> v veio do atual
-										gScore.put(v, tentativeScore); //atribui score da tentativa para evitar loop com o else if de cime
-										fScore.put(v, tentativeScore + heuristica(v)); //calcula a heuristica do nodo
+										veioDe.put(vizinho, atual); //atribui relacao pai e filho -> vizinho veio do atual
+										gScore.put(vizinho, tentativaScore); //atribui score da tentativa para evitar loop com o else if de cime
+										fScore.put(vizinho, tentativaScore + heuristica(vizinho)); //calcula a heuristica do nodo
 								});
 				}
 				_atual.ifPresent(this::reconstroePassos);
 		}
 
+		//monta a linked list do caminho percorrido, partindo do nodo final ate o nodo de inicio, utilizando o dicionario
 		public void reconstroePassos(Nodo nodo) {
-				path.add(nodo);
-				while (cameFrom.containsKey(nodo)) {
-						nodo = cameFrom.get(nodo);
-						path.add(0, nodo);
+				caminho.add(nodo);
+				while (veioDe.containsKey(nodo)) {
+						nodo = veioDe.get(nodo);
+						//adiciona sempre no index 0 pra ir "colocando na frente"
+						caminho.add(0, nodo);
 				}
 		}
 
 		private List<Nodo> getVizinhos(Nodo atual) throws Exception {
 				List<Nodo> vizinhos = new ArrayList<>();
 
-				if (atual.x - 1 >= 0 && maze[atual.x - 1][atual.y] != 1) {// esquerda
+				if (atual.x - 1 >= 0 && labirinto[atual.x - 1][atual.y] != 1) {// esquerda
 						vizinhos.add(new Nodo(atual.x - 1, atual.y));
 				}
-				if (atual.y + 1 < size && maze[atual.x][atual.y + 1] != 1) {// baixo
+				if (atual.y + 1 < tamanho && labirinto[atual.x][atual.y + 1] != 1) {// baixo
 						vizinhos.add(new Nodo(atual.x, atual.y + 1));
 				}
-				if (atual.x + 1 < size && maze[atual.x + 1][atual.y] != 1) {// direita
+				if (atual.x + 1 < tamanho && labirinto[atual.x + 1][atual.y] != 1) {// direita
 						vizinhos.add(new Nodo(atual.x + 1, atual.y));
 				}
-				if (atual.y - 1 >= 0 && maze[atual.y - 1][atual.x] != 1) {// cima
+				if (atual.y - 1 >= 0 && labirinto[atual.y - 1][atual.x] != 1) {// cima
 						vizinhos.add(new Nodo(atual.x, atual.y - 1));
 				}
 
 				return vizinhos;
 		}
 
-		//distancia
+		//distancia euclidiana para a funcao heuristica
 		public int heuristica(Nodo nodo) {
-				return Math.abs(nodo.y - end.y) + Math.abs(nodo.x - end.x);
+				return Math.abs(nodo.y - fim.y) + Math.abs(nodo.x - fim.x);
 		}
 
 		public void printMazeWithShortestPath() throws Exception {
-				for (int i = 0; i < maze.length; i++) {
-						for (int j = 0; j < maze[i].length; j++) {
-								if (path.contains(new Nodo(j, i))) {
+				for (int i = 0; i < labirinto.length; i++) {
+						for (int j = 0; j < labirinto[i].length; j++) {
+								if (caminho.contains(new Nodo(j, i))) {
 										System.out.print(ANSI_BLUE + "X");
-								} else if (maze[j][i] == 0) {
+								} else if (labirinto[j][i] == 0) {
 										System.out.print(ANSI_WHITE + "0");
-								} else if (maze[j][i] == 2 || maze[j][i] == 3) {
-										System.out.print(ANSI_YELLOW + maze[j][i]);
+								} else if (labirinto[j][i] == 2 || labirinto[j][i] == 3) {
+										System.out.print(ANSI_YELLOW + labirinto[j][i]);
 								} else {
-										System.out.print(ANSI_RED + maze[j][i]);
+										System.out.print(ANSI_RED + labirinto[j][i]);
 								}
 								System.out.print(" " + ANSI_RESET);
 						}
@@ -140,29 +142,29 @@ public class AEstrela {
 
 		public void printMazeWithAStarPath(Nodo n) throws Exception {
 				reconstroePassos(n);
-				for (int i = 0; i < maze.length; i++) {
-						for (int j = 0; j < maze[i].length; j++) {
-								if (path.contains(new Nodo(j, i))) {
+				for (int i = 0; i < labirinto.length; i++) {
+						for (int j = 0; j < labirinto[i].length; j++) {
+								if (caminho.contains(new Nodo(j, i))) {
 										System.out.print(ANSI_BLUE + "X");
-								} else if (maze[j][i] == 0) {
+								} else if (labirinto[j][i] == 0) {
 										System.out.print(ANSI_WHITE + "0");
-								} else if (maze[j][i] == 2 || maze[j][i] == 3) {
-										System.out.print(ANSI_YELLOW + maze[j][i]);
+								} else if (labirinto[j][i] == 2 || labirinto[j][i] == 3) {
+										System.out.print(ANSI_YELLOW + labirinto[j][i]);
 								} else {
-										System.out.print(ANSI_RED + maze[j][i]);
+										System.out.print(ANSI_RED + labirinto[j][i]);
 								}
 								System.out.print(" " + ANSI_RESET);
 						}
 						System.out.println(" " + ANSI_RESET);
 				}
-				path = new LinkedList<>();
+				caminho = new LinkedList<>();
 		}
 
 		private class Nodo {
 				public int x, y;
 
 				Nodo(int x, int y) throws Exception {
-						if (x < 0 || x >= size || y < 0 || y >= size)
+						if (x < 0 || x >= tamanho || y < 0 || y >= tamanho)
 								throw new Exception("Nodo invalido: x = " + x + "\n y = " + y);
 						this.x = x;
 						this.y = y;
--- t1/src/AG.java
+++ t1/src/AG.java
@@ -1,7 +1,10 @@
 package src;
 
 import java.time.LocalDateTime;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Random;
 import java.util.stream.Collectors;
 
 public class AG {
@@ -25,11 +28,16 @@ public class AG {
 				Cromossomo vencedor = null;
 				int i = 0;
 
+				//roda enquanto nao encontra a saida
 				while (!achouSaida) {
 
+						//calcula a aptidao de todos os cromossomos da populacao
 						populacao.forEach(this::aptidao);
 
+						//ordenada os cromossomos da populacao por aptida em order ascendente -> melhor primeiro
 						List<Cromossomo> ordenadaPorAptidao = populacao.stream().sorted(Comparator.comparing(Cromossomo::getScore)).collect(Collectors.toList());
+
+						//filtra dentro da populacao se algum cromossomo encontrou a saida -> se nao encontrou -> vencedor = null
 						vencedor = populacao.stream().filter(Cromossomo::isChegou).findFirst().orElse(null);
 						if (vencedor != null) {
 								achouSaida = true;
@@ -38,6 +46,7 @@ public class AG {
 								return vencedor;
 						}
 
+						//printa o andamento do algoritmo a cada X geracoes que recebeu por parametro
 						if (i % qtdGeracoesPrinte == 0) {
 								System.out.println("Geracao: " + i);
 								System.out.println("Melhor cromossomo com score: " + ordenadaPorAptidao.get(0).getScore());
@@ -46,12 +55,17 @@ public class AG {
 						}
 
 
+						//pega a metade da populacao com melhor aptidao
 						List<Cromossomo> metadeMaisApta = ordenadaPorAptidao.subList(0, ordenadaPorAptidao.size() / 2);
 						populacao.clear();
+
+						//depois de um grande numero de geracoes, sem ter encontrado a saida, reinicia a populacao, pois deve estar com a populacao toda muito similar
 						if (i != 0 && i % 150000 == 0) {
 								System.out.println(" \n RECOMECANDO.... \n");
 								metadeMaisApta = iniciaPopulacao().subList(0, ordenadaPorAptidao.size() / 2);
 						}
+
+						//faz o crossover utilizando a nova populacao, que eh a metade mais apta da populacao anterior, duplicada
 						populacao = crossOver(duplicaCromossomo(metadeMaisApta));
 						i++;
 				}
@@ -59,6 +73,7 @@ public class AG {
 
 		}
 
+		//duplica uma lista de cromossomos em uma nova lista
 		private List<Cromossomo> duplicaCromossomo(List<Cromossomo> c) {
 				List<Cromossomo> novaLista = new ArrayList<>();
 				c.forEach(it -> {
@@ -69,7 +84,7 @@ public class AG {
 		}
 
 
-		//21 agentes, com trajeto de tam 102 cada
+		//cria uma lista de Cromossomos, com trajeto randomizado, tamanho da populacao e tamanho do vetor de movimentos parametrizado
 		public List<Cromossomo> iniciaPopulacao() {
 				List<Cromossomo> populacao = new ArrayList<>();
 				for (int i = 0; i < tamanhoPopulacao; i++) {
@@ -79,7 +94,7 @@ public class AG {
 				return populacao;
 		}
 
-		//seta no cromossomo seu valor de aptidao
+		//calcula a aptidao somando a distancia euclidiana da ultima posicao do cromossomo ate a saida + penalidades que sofreu ao andar pelo labirinto
 		public int aptidao(Cromossomo cromossomo) {
 				Agente agente = new Agente(this.labirinto);
 				agente.percorrerTrajetoPeloLabirinto(cromossomo);
@@ -93,11 +108,7 @@ public class AG {
 				return score;
 		}
 
-		public int melhorScoreAptidao(List<Cromossomo> populacao) {
-				List<Integer> scores = populacao.stream().map(this::aptidao).collect(Collectors.toList());
-				return Collections.min(scores); //pega o maior numero do array
-		}
-
+		//faz a mutacao de um trajeto, se cair na porcentagem de chance de mutacao, pega uma posicao aleatoria do vetor de movimentos, e altera seu valor de forma randomica
 		public Cromossomo mutacao(int[] trajeto) {
 				if (r.nextInt(101) < chanceMutacao) {
 						trajeto[r.nextInt(trajeto.length)] = r.nextInt(4);
@@ -105,23 +116,32 @@ public class AG {
 				return new Cromossomo(trajeto);
 		}
 
-		//compara 2 agentes e retorna o com melhor score
+		//pega 2 cromossomos aleatorios da populacao e retorna o cromossomo com melhor score de aptidao
 		public Cromossomo torneio(List<Cromossomo> populacao) {
 				int l1 = r.nextInt(populacao.size());
 				int l2 = r.nextInt(populacao.size());
 				return populacao.get(l1).getScore() < populacao.get(l2).getScore() ? populacao.get(l1) : populacao.get(l2);
 		}
 
+		/*
+		 * Metodo que recebe uma populacao e retorna uma nova populacao, fazendo crossover, torneio e mutacao
+		 * Para cada novo cromossomo da nova populacao:
+		 * 	Seleciona 2 cromossomos da antiga populacao por torneio
+		 * 	Randomiza um indice que sera o ponto de corte para o crossover destes 2 cromossomos
+		 * 	Mergeia estes 2 cromossomos em um novo, sendo de 0 a ponto de corte, o cromossomo pai; sendo do ponto de corte ao final do trajto, o cromossomo mae;
+		 * 	Faz mutacao do trajeto deste novo cromossomo
+		 * 	Adiciona o novo cromossomo na nova populacao
+		 * */
 		public List<Cromossomo> crossOver(List<Cromossomo> populacao) {
 				List<Cromossomo> novaPopulacao = new ArrayList<>();
 
 				for (int i = 0; i < tamanhoPopulacao; i++) {
 						int[] pai;
 						int[] mae;
-						//do{
+
 						pai = torneio(populacao).getTrajeto();
 						mae = torneio(populacao).getTrajeto();
-						//}while(Arrays.equals(pai,mae));
+
 						int pontoCorte = r.nextInt(qtdMovimentos);
 
 						int[] novoTrajeto = new int[qtdMovimentos];
@@ -130,7 +150,7 @@ public class AG {
 						for (int j = 0; j < qtdMovimentos; j++) {
 								novoTrajeto[j] = j < pontoCorte ? pai[j] : mae[j];
 						}
-
+						//faz mutacao no novo cromossomo
 						Cromossomo novoCromossomo = mutacao(novoTrajeto);
 						novaPopulacao.add(novoCromossomo);
 				}
